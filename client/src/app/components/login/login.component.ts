b97ed19df96e9cfbea06958515d33c9ca29cf32c
MichalGoly <me@michalgoly.com>
2018-02-25 17:07:41 +0000

    created a basic login page for the student

---
+++ client/src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submitCode(): void {
+    window.alert("Woop");
+  }
+
+}
