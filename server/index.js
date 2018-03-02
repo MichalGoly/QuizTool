683350eeb259c7273f777593ed27bf10ba26d694
MichalGoly <me@michalgoly.com>
2018-03-02 15:42:55 +0000

    added some utility code to actually check if the image stored in db

--- server/index.js
+++ server/index.js
@@ -9,6 +9,27 @@ var db = require('./db/db');
 app.use('/lecturers', require('./controllers/lecturers.controller'));
 app.use('/lectures', require('./controllers/lectures.controller'));
 
+// TODO remove once no longer needed for dev purposes
+// var Slide = require('./models/slide');
+// app.get('/img', (req, res) => {
+//   Slide.find((err, slides) => {
+//     if (err) {
+//       res.send("Error: " + err);
+//     } else {
+//       res.send(slides);
+//     }
+//   });
+// });
+//
+// app.get('/img/:_id', (req, res) => {
+//   Slide.findById(req.params._id).then((slide) => {
+//     res.contentType('image/png');
+//     res.send(slide.image);
+//   }).catch((err) => {
+//     res.send("error: " + err);
+//   })
+// });
+
 app.get('/auth/google',
   passport.authenticate('google', {
     scope: ['email profile']
