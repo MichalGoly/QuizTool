51f80b2f0c4e7cf2224714f824445b3ad68bbe0d
MichalGoly <me@michalgoly.com>
2018-03-08 12:04:41 +0000

    clean up

--- server/helpers/socket.helper.js
+++ server/helpers/socket.helper.js
@@ -4,30 +4,37 @@ module.exports = function(server) {
   io = io(server);
 
   io.on('connection', (socket) => {
-    //log.info('socket.io connection made');
     console.log('socket.io connection made');
 
-    socket.on('event1', (data) => {
-      console.log(data.msg);
-    });
-
-    socket.emit('event2', {
-      msg: 'Server to client, do you read me? Over.'
-    });
-
-    socket.on('event3', (data) => {
-      console.log(data.msg);
-      socket.emit('event4', {
-        msg: 'Loud and clear :)'
+    socket.on('slide-update', (currentSlide) => {
+      console.log("[INFO] Socket.io event: slide-change");
+      socket.broadcast.emit('slide-change', {
+        img: currentSlide.img
       });
     });
 
-    socket.on('send-message', (message) => {
-      console.log("[INFO] send-message event received. Message: " + message.text);
-      socket.broadcast.emit('message-received', {
-        text: message.text
-      });
-    });
+    //
+    // socket.on('event1', (data) => {
+    //   console.log(data.msg);
+    // });
+    //
+    // socket.emit('event2', {
+    //   msg: 'Server to client, do you read me? Over.'
+    // });
+    //
+    // socket.on('event3', (data) => {
+    //   console.log(data.msg);
+    //   socket.emit('event4', {
+    //     msg: 'Loud and clear :)'
+    //   });
+    // });
+    //
+    // socket.on('send-message', (message) => {
+    //   console.log("[INFO] send-message event received. Message: " + message.text);
+    //   socket.broadcast.emit('message-received', {
+    //     text: message.text
+    //   });
+    // });
   });
 
   return io;
