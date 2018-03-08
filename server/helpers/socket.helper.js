20b7a9593a6ea0f3ed314d86a66aeaa86785fe5a
MichalGoly <me@michalgoly.com>
2018-03-08 11:54:46 +0000

    refactored socket.io out to its own file

---
+++ server/helpers/socket.helper.js
@@ -0,0 +1,34 @@
+var io = require('socket.io');
+
+module.exports = function(server) {
+  io = io(server);
+
+  io.on('connection', (socket) => {
+    //log.info('socket.io connection made');
+    console.log('socket.io connection made');
+
+    socket.on('event1', (data) => {
+      console.log(data.msg);
+    });
+
+    socket.emit('event2', {
+      msg: 'Server to client, do you read me? Over.'
+    });
+
+    socket.on('event3', (data) => {
+      console.log(data.msg);
+      socket.emit('event4', {
+        msg: 'Loud and clear :)'
+      });
+    });
+
+    socket.on('send-message', (message) => {
+      console.log("[INFO] send-message event received. Message: " + message.text);
+      socket.broadcast.emit('message-received', {
+        text: message.text
+      });
+    });
+  });
+
+  return io;
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var http = require('http');
-var io = require('socket.io');
 var app = express();
 var server = http.createServer(app);
 var passport = require('./helpers/passport.helper');
+var io = require('./helpers/socket.helper')(server);
 var db = require('./db/db');
+
+// socket.io setup
+app.use(function(req, res, next) {
+  req.io = io;
+  next();
+});
 
 app.use('/lecturers', require('./controllers/lecturers.controller'));
 app.use('/lectures', require('./controllers/lectures.controller'));
@@ -23,39 +29,6 @@ app.get('/auth/google/callback',
     res.cookie('auth', req.user.token);
     res.redirect('/');
   });
-
-/* setup socket.io */
-io = io(server);
-app.use(function(req, res, next) {
-  req.io = io;
-  next();
-});
-io.on('connection', (socket) => {
-  //log.info('socket.io connection made');
-  console.log('socket.io connection made');
-
-  socket.on('event1', (data) => {
-    console.log(data.msg);
-  });
-
-  socket.emit('event2', {
-    msg: 'Server to client, do you read me? Over.'
-  });
-
-  socket.on('event3', (data) => {
-    console.log(data.msg);
-    socket.emit('event4', {
-      msg: 'Loud and clear :)'
-    });
-  });
-
-  socket.on('send-message', (message) => {
-    console.log("[INFO] send-message event received. Message: " + message.text);
-    socket.broadcast.emit('message-received', {
-      text: message.text
-    });
-  });
-});
 
 server.listen('3000', () => {
   var host = server.address().address;
