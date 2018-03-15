1b33c11ef0e45d0f0b67b8060915d66cb0098f1c
MichalGoly <me@michalgoly.com>
2018-03-15 11:57:21 +0000

    added buttons to answer questions

--- client/src/app/components/lecture/answer/answer.component.ts
+++ client/src/app/components/lecture/answer/answer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { QuizService } from '../../../services/quiz.service';
+
 @Component({
   selector: 'lecture-answer',
   templateUrl: './answer.component.html',
@@ -10,9 +12,49 @@ export class AnswerComponent implements OnInit {
   @Input()
   currentSlide: Object;
 
-  constructor() { }
+  @Input()
+  socket: SocketIOClient.Socket;
+
+  options: string[];
+  chosenOption: string;
+  isSubmitted: boolean;
+
+  constructor(private quizService: QuizService) {
+    this.isSubmitted = false;
+  }
 
   ngOnInit() {
+    this.options = this.quizService.extractOptions(this.currentSlide["text"]);
+  }
+
+  choose(option: string): void {
+    this.handleSelection(option);
+    this.chosenOption = option;
+  }
+
+  submit(): void {
+    /*
+    * 1. Disable all buttons
+    * 2. Submit the chosenOption using sockets
+    */
+    console.log("Submitting: " + this.chosenOption);
+  }
+
+  handleSelection(option: string): void {
+    /*
+    * 1. Deselect previosuly selected buttons
+    * 2. Select the on passed into the method
+    */
+    for (let i = 0; i < this.options.length; i++) {
+      let element = $('#' + this.options[i]);
+      if (element.hasClass('yellow')) {
+        element.removeClass('yellow');
+      }
+      if (!element.hasClass('blue')) {
+        element.addClass('blue');
+      }
+    }
+    $('#' + option).addClass('yellow');
   }
 
 }
