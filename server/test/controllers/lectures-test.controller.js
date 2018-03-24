8b392d7171ef8218a4266ff7cce19e81b77d963b
MichalGoly <me@michalgoly.com>
2018-03-24 12:01:48 +0000

    added a failing test before implementing sessions cleanup on lecture removal

--- server/test/controllers/lectures-test.controller.js
+++ server/test/controllers/lectures-test.controller.js
@@ -7,6 +7,7 @@ var expect = chai.expect;
 var Lecturer = require('../../models/lecturer');
 var Lecture = require('../../models/lecture');
 var Slide = require('../../models/slide');
+var Session = require('../../models/session');
 
 chai.use(chaiHttp);
 
@@ -161,5 +162,97 @@ describe('test lectures controller', () => {
       assert.fail(1, 0, err);
       done();
     });
+  });
+
+  it('should remove lecture, slides and sessions when valid id and user authorized', (done) => {
+    chai.request(app).post('/lectures').attach('file', __dirname + '/../bin/presentation.pdf')
+      .end((err, res) => {
+        res.should.have.status(201);
+        Lecture.find({}).then((lectures) => {
+          var lect = lectures[0];
+          assert.equal(lect["fileName"], "presentation.pdf");
+          Slide.find({
+            lectureId: lect._id
+          }).then((slides) => {
+            slides[1].quizType = "single";
+            slides[1].save().then((slide1) => {
+              slides[3].quizType = "multi";
+              slides[3].save().then((slide3) => {
+                slide1_id = slide1._id;
+                slide3_id = slide3._id;
+                const newSession = {
+                  lectureId: lect._id,
+                  date: Date("Sat Mar 24 2018 08:47:09 GMT+0000 (GMT)"),
+                  answers: {
+                    slide1_id: {
+                      "B": 10,
+                      "C": 1,
+                      "correct": "C"
+                    },
+                    slide3_id: {
+                      "A": 2,
+                      "AB": 20,
+                    }
+                  }
+                };
+                chai.request(app).post('/sessions').set('content-type', 'application/json').send(newSession).end((err, res) => {
+                  res.should.have.status(201);
+                  chai.request(app).get('/sessions/' + lect._id).end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.length.should.be.eql(1);
+                    res.body[0].should.have.property('_id');
+                    res.body[0].should.have.property('lectureId').eql(lect._id.toString());
+                    res.body[0].should.have.property('answers').eql({
+                      slide1_id: {
+                        "B": 10,
+                        "C": 1,
+                        "correct": "C"
+                      },
+                      slide3_id: {
+                        "A": 2,
+                        "AB": 20,
+                      }
+                    });
+                    res.body[0].should.have.property('date');
+
+                    chai.request(app).delete('/lectures/' + lect._id).end((err, res) => {
+                      res.should.have.status(200);
+                      Slide.find({
+                        lectureId: lect._id
+                      }).then((slides) => {
+                        assert.isEmpty(slides);
+                        Session.find({
+                          lectureId: lect._id
+                        }).then((sessions) => {
+                          assert.isEmpty(sessions);
+                          done();
+                        }).catch((err) => {
+                          assert.fail(1, 0, err);
+                          done();
+                        });
+                      }).catch((err) => {
+                        assert.fail(1, 0, err);
+                        done();
+                      });
+                    });
+                  });
+                });
+              }).catch((err) => {
+                assert.fail(0, 1, err);
+                done();
+              })
+            }).catch((err) => {
+              assert.fail(0, 1, err);
+              done();
+            });
+          }).catch((err) => {
+            assert.fail(0, 1, err);
+            done();
+          });
+        }).catch((err) => {
+          assert.fail(0, 1, err);
+          done();
+        });
+      });
   });
 });
