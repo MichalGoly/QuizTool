edfde1301c4b20be9bf4b83f76d8cd7376bb9162
MichalGoly <me@michalgoly.com>
2018-04-01 17:41:03 +0200

    tested AnswersComponent

--- client/src/app/components/lecture/answer/answer.component.spec.ts
+++ client/src/app/components/lecture/answer/answer.component.spec.ts
@@ -27,10 +27,24 @@ describe('AnswerComponent', () => {
       text: "Lecture 2 - Java Basics",
       quizType: null
     }
+    component.sessionCode = "das2sa2as";
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should check if valid', () => {
+    expect(component.isValid(null)).toBe(false);
+    expect(component.isValid({})).toBe(false);
+    expect(component.isValid({ "hello": 4231 })).toBe(false);
+    expect(component.isValid({
+      "sessionCode": "das2sa2as",
+      "options": ["A", "C"]
+    })).toBe(true);
+    expect(component.isValid({
+      "options": ["A", "C"]
+    })).toBe(false);
+  })
 });
