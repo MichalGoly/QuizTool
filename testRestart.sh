91c550433dcd0881430226a471099d561427cb1c
MichalGoly <me@michalgoly.com>
2018-04-03 10:56:08 +0200

    Moved integration tests to a newly created tester container to avoid nginx 502 nightmare

--- testRestart.sh
+++ testRestart.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-# echo "Running client unit tests"
-# cd client
-# docker build -t clienttest --file Dockerfile.test .
-# docker run -it clienttest
-#
-# # Server side unit tests
-# echo "Running server unit tests"
-# cd ..
-# docker-compose -f docker-compose.test.yml down
-# docker-compose -f docker-compose.test.yml build
-# docker-compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from server_node
-# docker-compose -f docker-compose.test.yml down
+echo "Running client unit tests"
+cd client
+docker build -t clienttest --file Dockerfile.test .
+docker run -it clienttest
+
+# Server side unit tests
+echo "Running server unit tests"
+cd ..
+docker-compose -f docker-compose.test.yml down
+docker-compose -f docker-compose.test.yml build
+docker-compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from server_node
+docker-compose -f docker-compose.test.yml down
 
 echo "Running integration tests"
 docker-compose -f docker-compose.integration.yml down
 docker-compose -f docker-compose.integration.yml build
-docker-compose -f docker-compose.integration.yml up --abort-on-container-exit --exit-code-from server_node
+docker-compose -f docker-compose.integration.yml up --abort-on-container-exit --exit-code-from tester
 docker-compose -f docker-compose.integration.yml down
